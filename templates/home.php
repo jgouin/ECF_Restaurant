156f89478b1a3a3afb3759defdbeffd52710d892
Julie Gouin <juliegouin@outlook.com>
2023-02-22 10:58:29 +0100

    first commit

---
+++ templates/home.php
@@ -0,0 +1,59 @@
+<div class="row text-center">
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/1-1984.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/2-histoires-courtes.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/3-zai-zai-zai-zai.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/1-1984.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/2-histoires-courtes.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src=" uploads/books/3-zai-zai-zai-zai.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
